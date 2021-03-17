e4269260904ca1032da7e53dc550adc794a8b9bc
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-03-16 23:30:26 -0300

    feat: add input type entry

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container, TransactionTypeContainer } from './styles'
 
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionModalProps{
   isOpen:boolean,
@@ -25,6 +27,22 @@ export function NewTransactionModal({isOpen,onRequestClose}:NewTransactionModalP
         <h2>Cadastrar transação</h2>
         <input type="text" placeholder="Titulo"/>
         <input type="number" placeholder="Valor"/>
+
+        <TransactionTypeContainer>
+          <button type="button">
+            
+            <img src={incomeImg} alt="Entrada"/>
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            
+            <img src={outcomeImg} alt="Saida"/>
+            <span>Saida</span>
+          </button>
+
+        </TransactionTypeContainer>
+
         <input type="text" placeholder="Categoria"/>
         <button type="submit">Cadastrar</button>
       </Container>
